da25ee0f6923adb849e7917d32a730df4e9b9d4f
Shemaremy <remyshema20@gmail.com>
2023-11-26 15:56:20 +0200

    nbghb

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,9 @@
+#
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+number=random.random()
+if number > 0:
+    print(number, "Is positive")
+else:
+    print(number, "Is negative")
