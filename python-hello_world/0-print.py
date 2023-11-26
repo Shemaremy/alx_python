3893f017defb2c4b28453cfbf192accdb53a0bd9
Shemaremy <remyshema20@gmail.com>
2023-11-26 15:36:24 +0200

    ygyh

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,1 +1,3 @@
-print("\"Programming is like building a multilingual puzzle")
+#print("\"Programming is like building a multilingual puzzle")
+
+print("Hey im " , 4)
---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,7 @@
+#
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: " , word[:3])
+print("Last 2 letters: " , word[7:])
+print("Middle word: " , word[1:-1])
+
