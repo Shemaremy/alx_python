bb2d3fe718b0bec1297c30752edb9b087d020909
Shemaremy <remyshema20@gmail.com>
2023-11-26 15:38:55 +0200

    HGHBSX

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 #
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: " , word[:3])
-print("Last 2 letters: " , word[7:])
-print("Middle word: " , word[1:-1])
+print("First 3 letters:",word[:3])
+print("Last 2 letters: ",word[7:])
+print("Middle word: ",word[1:-1])
 
