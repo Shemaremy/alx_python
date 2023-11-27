5474ea710064d829d4db178715928b1df7cd1206
Shemaremy <remyshema20@gmail.com>
2023-11-27 11:56:00 +0200

    umuntu

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,16 @@
+#
+import random
+
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+
+ if number > 0:
+    print(number, "is positive")
+
+else if number < 0:
+    print(number, "is negative")
+
+    else:
+    print(number, "is zero")
+
+    
