31f7c9e010d6eedba32ffcef57df5f8412b7033a
Shemaremy <remyshema20@gmail.com>
2023-11-27 15:22:16 +0200

    jhj

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,6 @@
 for a in range(0,100,1):
     if a < 10:
          print("0{}".format(a), end=", ")
-    elif a>=10 and a<99:
+    elif a>=10:
         print(a, end=", ")
-    elif a==99:
-        print(a)
+    
