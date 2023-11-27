32f12b01265b703c0f0176686224027a5b994c7f
Shemaremy <remyshema20@gmail.com>
2023-11-27 14:15:10 +0200

    hbhbh

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,11 +4,13 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 if number > 0:
      last = number%10
-      
-     if last == 0:
-         print("The last digit of" , number , "is" , last, "and is 0" )
-     elif last > 5:
+
+     if last > 5:
          print("The last digit of" , number , "is" , last, "and is greater than 5" )
+
+     elif last == 0:
+         print("The last digit of" , number , "is" , last, "and is 0" )
+     
      elif last < 6 and last > 0:
          print("The last digit of" , number , "is" , last, "and is less than 6 not 0" )
 
