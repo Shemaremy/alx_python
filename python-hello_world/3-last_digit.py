e2711a8825d8295cee0d9f6c805d9190896b2805
Shemaremy <remyshema20@gmail.com>
2023-11-27 14:16:36 +0200

    uhhu

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,22 +6,22 @@ if number > 0:
      last = number%10
 
      if last > 5:
-         print("The last digit of" , number , "is" , last, "and is greater than 5" )
+         print("Last digit of" , number , "is" , last, "and is greater than 5" )
 
      elif last == 0:
-         print("The last digit of" , number , "is" , last, "and is 0" )
+         print("Last digit of" , number , "is" , last, "and is 0" )
      
      elif last < 6 and last > 0:
-         print("The last digit of" , number , "is" , last, "and is less than 6 not 0" )
+         print("Last digit of" , number , "is" , last, "and is less than 6 not 0" )
 
 
 elif number < 0:
      last = number % -10
      if last == 0:
-         print("The last digit of" , number , "is" , last, "and is 0" )
+         print("Last digit of" , number , "is" , last, "and is 0" )
      elif last > 5:
-         print("The last digit of" , number , "is" , last, "and is greater than 5" )
+         print("Last digit of" , number , "is" , last, "and is greater than 5" )
      elif last < 6 and last > 0:
-         print("The last digit of" , number , "is" , last, "and is less than 6 not 0" )
+         print("Last digit of" , number , "is" , last, "and is less than 6 not 0" )
 
 
