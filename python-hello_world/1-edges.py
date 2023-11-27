10690ccc2dfaba0e32e089cb8f26e4658d0fc18a
Shemaremy <remyshema20@gmail.com>
2023-11-27 12:19:11 +0200

    mmmmm

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,4 @@ middle_word = school[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-print("8 1-edges.py")
+ 
