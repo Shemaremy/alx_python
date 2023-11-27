28a45eb51f344965447f1d70731fd5c0aa41dff2
Shemaremy <remyshema20@gmail.com>
2023-11-27 12:21:07 +0200

    teslaaaaaaaa

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
-school = "Holberton"
+school = "School"
 word_first_3 = school[:3]
-word_last_2 = school[7:]
+word_last_2 = school[4:]
 middle_word = school[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
