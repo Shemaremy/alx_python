bbff0a0be26d4f7a3ef07e163e2eaa8042977289
Shemaremy <remyshema20@gmail.com>
2023-11-29 13:27:42 +0200

    Password Validator

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,2 +1,16 @@
-def validate_password(password)
+def validate_password(password):
+ 
+     if any(i.isalpha()==False for i in password) and any(i.isspace()!=True for i in password) and len(password)>=8:
+
+         if any(char.isupper() for char in password) and any(char.islower() for char in password):              
+              m="True"
+         else:                                
+              m="False"
+
+     else:
+             m="False"
+
+
+     return m            
+        
 
---
+++ python-functions/delete this 2.py
@@ -0,0 +1,12 @@
+a = "AAAAAAAAAAAAAAi"
+
+if any(char.isupper() for char in a) and any(char.islower() for char in a):              
+    print("Very good")
+    
+else:                                
+    print("Contanin a small and a big please")
+    
+    
+
+
+
