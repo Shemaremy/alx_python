1c68d1ca40f9023e01cd70afee1b3f8c37822366
Shemaremy <remyshema20@gmail.com>
2023-12-02 11:34:42 +0200

    external functions

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,5 @@
-__import__("0-add")
-import add_0
-print("1 + 2 = {}".format(add_0.add(1,2)))
+if __name__ == "__main__":
 
+ __import__("0-add")
+ import add_0
+ print("1 + 2 = {}".format(add_0.add(1,2)))
