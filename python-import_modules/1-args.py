15c1bd377789190ec121e943c0d710a34fc60187
Shemaremy <remyshema20@gmail.com>
2023-12-04 20:46:40 +0200

    Count arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,7 +1,7 @@
 from args_1 import numberofargs
 if __name__ == "__main__":
     
-    pets = []
+    pets = ["Hello","Holberton","School","98","Battery","street"]
     result = numberofargs(pets)
     if result is not None :
        print(numberofargs(pets))
