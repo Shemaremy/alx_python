18d0225ff8e4764b02a404d966045c07c4f8b2ad
Shemaremy <remyshema20@gmail.com>
2023-12-04 19:47:49 +0200

    Import then add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,8 +5,5 @@ b= 2
 
 if __name__ == "__main__":
  
- if a==1 and b==2:
-    print("1 + 2 = {}".format(add(a,b)))
- else:
-    print("{} + {} = {}".format((a,b),add(a,b)))
+  print("{} + {} = {}".format(a,b,add(a,b)))
 
