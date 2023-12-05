bb6948e5151df82347c0faa11b5f91b22f9fa355
Shemaremy <remyshema20@gmail.com>
2023-12-05 12:06:18 +0200

    divide integers

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,39 +1,25 @@
 a = 10
 b = 2
 
-class Flacko(Exception):
- """Custom exception for negative values."""
-
 def safe_print_division(a, b):
      if b<0:
-       
-       
       x="Inside reslt: {}".format(a/b)
       c=a/b
      elif b>0:
        
-       
       x="Inside lt: {}".format(a/b)
       c=a/b
-    
-     return c 
+      return c 
 
 try:
-
-    print("Inside result: {}".format(safe_print_division(a,b)))
-
-    
-    
-    #print(a if a>b else b)
+    a = 10
+    b = 2
 
 except ZeroDivisionError:
     print("Inside result None")
 
 finally:
-   pass   
+   print("Inside result: {}".format(safe_print_division(a,b)))
 
- #a = 10
- #b = 2
-#result = safe_print_division(a, b)
-#print("{:d} / {:d} = {}".format(a, b, result))
+ 
 
