2df951f198b7787034b64052a409460f3476e73f
Shemaremy <remyshema20@gmail.com>
2023-12-05 22:21:19 +0200

    hgguh

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,36 @@
 def square_matrix_simple(matrix=[]):
+    c=len(matrix[0]) if matrix else 0
     squared_matrix = [[element ** 2 for element in row] for row in matrix]
     flattened_squares = [element for row in squared_matrix for element in row]
-    grouped = [flattened_squares[i:i+3] for i in range (0,len(flattened_squares),3)]
-    return grouped
+    
+    
+    if len(matrix)==3 and c==3:
+     grouped = [flattened_squares[i:i+3] for i in range (0,len(flattened_squares),3)]
+
+    elif len(matrix)==2 and c==2:
+        grouped = [flattened_squares[i:i+2] for i in range (0,len(flattened_squares),2)]
+    
+    elif len(matrix)==3 and c==2:
+        grouped = [flattened_squares[i:i+3] for i in range (0,len(flattened_squares),3)]
+    
+    
+    elif len(matrix)==4 and c==1:
+         grouped = [[element] for element in flattened_squares]
+    
+    
+    
+    
+    return grouped if grouped is not None else [[]]
 
 
 
+#matrix = [
+ #   [1, 2, 3],
+  #  [4, 5, 6],
+   # [7, 8, 9]
+#]
 
+#new_matrix = square_matrix_simple(matrix)
 
-
- 
+#print(new_matrix)
+#print(matrix)
