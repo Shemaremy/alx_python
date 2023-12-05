b71c4e5a2c8e4fee2bc829a78520263ced8cc125
Shemaremy <remyshema20@gmail.com>
2023-12-05 21:05:24 +0200

    hhij

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
         return(len(sentence), sentence[0])
 
 
-#sentence = "Holberton"
-#length, first = multiple_returns(sentence)
-#print("Length: {:d} - First character: {}".format(length, first))
+sentence = ""
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,12 @@
+def square_matrix_simple(matrix=[]):
+    squared_matrix = [[element ** 2 for element in row] for row in matrix]
+    flattened_squares = [element for row in squared_matrix for element in row]
+    grouped = [flattened_squares[i:i+3] for i in range (0,len(flattened_squares),3)]
+    return grouped
 
+
+
+
+
+
+ 
