d0cc33506deca55c34a545a02c1812efd407a2c5
Shemaremy <remyshema20@gmail.com>
2023-12-05 11:46:38 +0200

    ttyy6

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,13 +5,17 @@ class Flacko(Exception):
 
 def safe_print_division(a, b):
      if b<0:
+      a=10
+      b=-2
       x="Inside reslt: {}".format(a/b)
-      p=a/b
+      c=a/b
      elif b>0:
+      a=10
+      b=2
       x="Inside lt: {}".format(a/b)
-      p=a/b
+      c=a/b
     
-     return p 
+     return a/b 
 
 try:
     pass
@@ -24,7 +28,7 @@ except ZeroDivisionError:
     print("Inside result None")
 
 finally:
-   u = safe_print_division(10,2)
+   u = safe_print_division(12,5)
    if u is not None:
     print("Inside result:",u)
 
