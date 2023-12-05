4cc0ef0638056ff90cf011af1d575beaf72e7828
Shemaremy <remyshema20@gmail.com>
2023-12-05 19:50:12 +0200

    fdfr

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,11 +2,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         print("{:d} {:d} {:d}".format(*row))
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+ 
 
 
 print_matrix_integer(matrix)
