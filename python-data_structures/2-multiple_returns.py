a911ea782e5c0127cae976d4c146838d887af03b
Shemaremy <remyshema20@gmail.com>
2023-12-05 20:42:33 +0200

    uhuu

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,10 @@
+def multiple_returns(sentence):
+    if not sentence:
+        return (None, None)
+    else:
+        return(len(sentence), sentence[0])
+
+
+#sentence = "Holberton"
+#length, first = multiple_returns(sentence)
+#print("Length: {:d} - First character: {}".format(length, first))
