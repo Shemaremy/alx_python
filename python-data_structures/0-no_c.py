287d5db26412d69d8cf37fce722fbe8f3ef56d76
Shemaremy <remyshema20@gmail.com>
2023-12-05 19:12:34 +0200

    Chicago fun

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,1 +1,8 @@
+def no_c(my_string):
+    a=''.join(char for char in my_string  if char.lower() not in {'c'})
+    return a
 
+
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
