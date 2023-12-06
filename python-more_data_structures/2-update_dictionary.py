4a8941176877dd19f4ac95da06f9aa1b60122b90
Shemaremy <remyshema20@gmail.com>
2023-12-06 12:12:12 +0200

    deef

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,11 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+
+#my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
+#key = "a"
+#value = "A"
+#new_dict = update_dictionary(my_dict, key, value)
+#print_sorted_dictionary(new_dict)
+#print("xx")
+#print_sorted_dictionary(my_dict)
