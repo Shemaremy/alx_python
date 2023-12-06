b5e0d18f709c68b2ddd617a3943554fc18a5b9c5
Shemaremy <remyshema20@gmail.com>
2023-12-06 11:21:56 +0200

    rfrfc

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -10,7 +10,6 @@ def safe_print_division(a, b):
       x="Inside lt: {}".format(a/b)
       c=a/b
       return c 
-
 try:
     a = 10
     b = 2
@@ -19,6 +18,12 @@ except ZeroDivisionError:
     print("Inside result None")
 
 finally:
+
+ a = 10
+ b = -2
+ result = safe_print_division(a, b)
+ print("{:d} / {:d} = {}".format(a, b, result))
+
    print("Inside result: {}".format(safe_print_division(a,b)))
 
  
---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,12 @@
+def common_elements(set_1, set_2):
+  return set_1.intersection(set_2) 
+
+
+
+
+
+
+#set_1 = { "Python", "C", "Javascript" }
+#set_2 = { "Bash", "C", "Python", "Perl" }
+#c_set = common_elements(set_1, set_2)
+#print(sorted(list(c_set)))
