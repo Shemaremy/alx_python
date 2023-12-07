2c6a4fcb38642ffb88e5c9f5835d9d88803fd7da
Shemaremy <remyshema20@gmail.com>
2023-12-07 11:23:38 +0200

    njnnuu

--- python-import_modules/args_1.py
+++ python-import_modules/args_1.py
@@ -8,7 +8,7 @@ def print_arguments(args_list):
         print(f"{c} argument{'' if c == 1 else 's'}:")
 
         for i, arg in enumerate(args_list, start=1):
-            print(f"{i}: {repr(arg)}") 
+            print(f"{i}: {arg}") 
 
 
 
