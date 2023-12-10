9926d3f3d2b1825447acd9fbe0f668fa2770d129
Shemaremy <remyshema20@gmail.com>
2023-12-11 00:57:34 +0200

    jjswsd

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -5,9 +5,11 @@ class Remi:
     pass
 
 a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+
+
+#if is_same_class(a, int):
+ #   print("{} is an instance of the class {}".format(a, int.__name__))
+#if is_same_class(a, float):
+ #   print("{} is an instance of the class {}".format(a, float.__name__))
+#if is_same_class(a, object):
+ #   print("{} is an instance of the class {}".format(a, object.__name__))
