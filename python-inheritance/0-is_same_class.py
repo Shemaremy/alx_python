e46b6ec3c54bf116e2d4512aec3cc30f463054ce
Shemaremy <remyshema20@gmail.com>
2023-12-11 01:25:36 +0200

    njnjn

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,15 +1,12 @@
-""" Here  is everything """
+
 def is_same_class(obj, a_class):
-""" Here  is everything """
 
   class Remi:
-    """ Here  is everything """
      pass
-""" Here  is everything """
+
   return isinstance(obj, a_class)
 
-""" Here  is everything """
 a = 1
-print(a)
+#print(is_same_class(a, int))
 
  
