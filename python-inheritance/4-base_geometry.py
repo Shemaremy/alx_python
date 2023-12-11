03cbfe468b20f9149061281b83b71e9a80456e80
Shemaremy <remyshema20@gmail.com>
2023-12-11 22:56:07 +0200

    commit 432

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,13 @@
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+'''
+class BaseGeometry:
+    '''
+    The below program will be
+    telling more about inhertitance
+    in python programming
+    '''  
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
