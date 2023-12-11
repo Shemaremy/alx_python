80b4cb9f9b22079fb2cd5571739334f3dd52e260
Shemaremy <remyshema20@gmail.com>
2023-12-11 11:26:08 +0200

    Size of square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,16 @@
 #class definition
+
 class Square:
+    
     def __init__(self, _Square__size):
         #constructor definition by using init
+    
         self._Square__size = _Square__size
         #Initialisation 
 
     def get_size(self):
-        #A method definition to return everything we need
+    
+        #A method to return the sixe of our square
         return self._Square__size
 
 #Instantiation
