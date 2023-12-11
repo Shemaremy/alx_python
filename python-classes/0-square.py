55571c73c040083b71bde024bbe3ccc2f6bc9cbb
Shemaremy <remyshema20@gmail.com>
2023-12-11 11:05:45 +0200

    hbjhi

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,12 @@
+class Square:
+    def init(self, size):
+        self.size = size
 
+    def get_size(self):
+        return self.size
+
+#Instantiation
+my_square = Square(5)
+
+#Accessing the private attribute using a method
+print("Size of the square:", my_square.get_size())
