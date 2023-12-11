058890c0cf3873614b5b4cf6b7dd3931495b8fae
Shemaremy <remyshema20@gmail.com>
2023-12-11 22:49:08 +0200

    hghjns

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,4 +4,9 @@ telling more about inhertitance
 in python programming
 '''
 class BaseGeometry:
- 
+    '''
+    The below program will be
+    telling more about inhertitance
+    in python programming
+    '''
+    pass
