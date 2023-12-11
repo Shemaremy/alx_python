8fb5d6238f76a96d3ac9765bae62e1cc4ca2526f
Shemaremy <remyshema20@gmail.com>
2023-12-11 12:09:02 +0200

    Commit 509

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,22 @@
+'''
+The below program will be
+telling us the size of our square
+'''
+
 class Square:
+    '''
+    Here comes a class definition
+    which contais objects
+    such as the methods you're seeing below
+    '''
     def __init__(self, size=0):
         # Validate if size is an integer
         if not isinstance(size, int):
-            raise TypeError("size must be an integer.")
+            raise TypeError("size must be an integer")
 
         # Validate if size is greater than or equal to zero
         if size < 0:
-            raise ValueError("size must be greater or equal to zero.")
+            raise ValueError("size must be >= 0")
 
         # If validations pass, set the size as a private instance attribute
         self.__size = size
@@ -20,15 +30,15 @@ try:
     # Create a Square instance with size 5
     square_instance = Square(5)
     # Calculate and print the area
-    print("Area of the square:", square_instance.area())
+    
 
     # Attempt to create a Square instance with a non-integer size
     invalid_square = Square("invalid")
 except TypeError as e:
-    print(f"Error: {e}")
+    pass
 
 try:
     # Attempt to create a Square instance with a negative size
     negative_square = Square(-3)
 except ValueError as e:
-    print(f"Error: {e}")
+    pass
