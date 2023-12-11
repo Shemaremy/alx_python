75fad2425110b82b3daaf68498fe027647e5cf68
Shemaremy <remyshema20@gmail.com>
2023-12-11 23:04:32 +0200

    commit 654

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+'''
+class BaseGeometry:
+    '''
+    The below program will be
+    telling more about inhertitance
+    in python programming
+    '''  
+    def area(self):
+        raise NotImplementedError("area() is not implemented.")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer.")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than zero.")
