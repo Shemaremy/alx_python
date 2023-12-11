c467ea224f74c9f15bfc9c70d91ea85e17b90583
Shemaremy <remyshema20@gmail.com>
2023-12-11 11:51:17 +0200

    commit 508

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,11 +2,11 @@ class Square:
     def __init__(self, size=0):
         # Validate if size is an integer
         if not isinstance(size, int):
-            raise TypeError("size must be an integer.")
+            raise TypeError
 
         # Validate if size is greater than or equal to zero
         if size < 0:
-            raise ValueError("size must be greater or equal to zero.")
+            raise ValueError
 
         # If validations pass, set the size as a private instance attribute
         self.__size = size
@@ -18,15 +18,15 @@ class Square:
 try:
     # Create a Square instance with size 5
     square_instance = Square(5)
-    print("Size of the square:", square_instance.get_size())
+    
 
     # Attempt to create a Square instance with a non-integer size
     invalid_square = Square("invalid")
 except TypeError as e:
-    print(f"Error: {e}")
+    pass
 
 try:
     # Attempt to create a Square instance with a negative size
     negative_square = Square(-3)
 except ValueError as e:
-    print(f"Error: {e}")
+    pass
