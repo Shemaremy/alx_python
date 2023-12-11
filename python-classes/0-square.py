e5699c885fea4bb4ae911ebe4cfef920917092c6
Shemaremy <remyshema20@gmail.com>
2023-12-11 11:15:00 +0200

    jnjkkkkkl

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,7 +6,11 @@ class Square:
         return self.size
 
 #Instantiation
-my_square = Square(5)
+#my_square = Square(5)
 
 #Accessing the private attribute using a method
-print("Size of the square:", my_square.get_size())
+#print("Size of the square:", my_square.get_size())
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
