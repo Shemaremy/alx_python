f034828229f503753e3b6ddab1d40d39f91cf089
Shemaremy <remyshema20@gmail.com>
2023-12-11 22:46:16 +0200

    commit 403

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,12 @@
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+'''
+class BaseGeometry:
+    pass
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+'''
