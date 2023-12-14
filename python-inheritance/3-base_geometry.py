0113394423b8cd9e965143e70a7e3c28579652ca
Shemaremy <remyshema20@gmail.com>
2023-12-14 12:11:57 +0200

    kmkmn

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,8 +6,7 @@ in python programming
 class BaseGeometry:
    class meta_class:
     pass
-    #def __init_subclass__(cls,**kwargs):
-     #   pass
+
     def __dir__(self):
         return [attr for attr in dir(type(self)) if not (attr.startswith("__") and attr.endswith("__")) or attr == "__dir__"]
 
