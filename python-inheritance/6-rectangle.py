aa95b16886776c6dd0a5657bb1f9d60afa27db71
Shemaremy <remyshema20@gmail.com>
2023-12-14 15:36:30 +0200

    commit 403

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,41 +1,45 @@
+
 class BaseGeometry:
     '''
     This is the Class docstring
-    (Tring to figure out something. lol)
+    (Trying to figure out something. lol)
     '''
     pass
 
 '''
 The below program will be
-telling more about inhertitance
-in python programming
+telling more about inheritance
+in Python programming
 '''
 class Rectangle(BaseGeometry):
-    
- '''
- The below program will be
- telling more about inhertitance
- in python programming
- '''
- def __init__(self, width, height):
     '''
-    initialises an instance of the class rectangle
-    Okay
+    The below program will be
+    telling more about inheritance
+    in Python programming
     '''
-    self.integer_validator("width", width)
-    self.integer_validator("height", height)
-    self.__width = width
-    self.__height = height
+    def __init__(self, width, height):
+        '''
+        Initializes an instance of the class rectangle
+        Okay
+        '''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
- def integer_validator(self, name, value):
-    if not isinstance (value, int) or value <= 0:
-        raise ValueError("width must be greater than 0")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int) or value <= 0:
+            if name == "height":
+                raise TypeError("height must be an integer")
+            elif name == "width":
+                raise TypeError("width must be greater than 0")
 
-
+#try:
+#    r = Rectangle(3, "3")
+#except TypeError as e:
+#    print("[{}] {}".format(e.__class__.__name__, e))
 
 #try:
 #    r = Rectangle(0, 4)
-#except Exception as e:
+#except TypeError as e:
 #    print("[{}] {}".format(e.__class__.__name__, e))
-
-#print(issubclass(Rectangle, BaseGeometry))
