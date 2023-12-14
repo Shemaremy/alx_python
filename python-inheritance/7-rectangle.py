9ddb6f9f2a0c6b87cc2db00a600822e4d22b1c5a
Shemaremy <remyshema20@gmail.com>
2023-12-14 17:16:42 +0200

    commit 398

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -21,7 +21,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer.")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than zero.")
 
