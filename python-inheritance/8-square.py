78eb22f2f0c480764043debfa9f4831612a1a2ca
Shemaremy <remyshema20@gmail.com>
2023-12-14 17:27:37 +0200

    commit 390

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,42 @@
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+
+'''
+class Rectangle:
+    '''
+    The below program will be
+    telling more about  inhertitance
+    in python programming
+    '''
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Square(Rectangle):
+    '''
+    The below program will be
+    telling more about inhertitance
+    in python programming
+    '''
+    def __init__(self, size):
+        self.__integer_validator(size)
+        super().__init__(size, size)
+
+    @staticmethod
+    def __integer_validator(value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Size must be a positive integer")
+
+
+# Example usage:
+try:
+    square = Square(5)
+    print(f"Square area: {square.area()}")
+except ValueError as e:
+    print(f"Error: {e}")
