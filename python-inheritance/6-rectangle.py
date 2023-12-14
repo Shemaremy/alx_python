2759887d52cc26b18711467fedae4c2eb43c97ae
Shemaremy <remyshema20@gmail.com>
2023-12-14 14:05:40 +0200

    commit 392

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,9 +1,16 @@
+class BaseGeometry:
+    '''
+    This is the Class docstring
+    (Tring to figure out something. lol)
+    '''
+    pass
+
 '''
 The below program will be
 telling more about inhertitance
 in python programming
 '''
-class Rectangle:
+class Rectangle(BaseGeometry):
  '''
  The below program will be
  telling more about inhertitance
@@ -15,6 +22,15 @@ class Rectangle:
     Okay
     '''
     self.integer_validator("width", width)
-    self.integer_validator("width", height)
+    self.integer_validator("height", height)
     self.__width = width
     self.__height = height
+
+ def integer_validator(self, name, value):
+    if not isinstance (value, int):
+        raise TypeError(f"{name} must be an integer")
+
+
+
+
+#print(issubclass(Rectangle, BaseGeometry))
