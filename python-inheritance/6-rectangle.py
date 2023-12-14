0f4a80b119517984238f4495af6c32dc354dbba1
Shemaremy <remyshema20@gmail.com>
2023-12-14 13:41:07 +0200

    commit 302

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -3,30 +3,18 @@ The below program will be
 telling more about inhertitance
 in python programming
 '''
-class BaseGeometry:
+class Rectangle:
+ '''
+ The below program will be
+ telling more about inhertitance
+ in python programming
+ '''
+ def __init__(self, width, height):
     '''
-    The below program will be
-    telling more about inhertitance
-    in python programming
-    '''  
-
-def area(self):
-        raise NotImplementedError("area() is not implemented.")
-
-def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer.")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than zero.")
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
-
-
-try:
-    rectangle = Rectangle(5, 10)
-    print("Rectangle created successfully.")
-except Exception as e:
-    print(f"Error: {e}")
+    initialises an instance of the class rectangle
+    Okay
+    '''
+    self.integer_validator("width", width)
+    self.integer_validator("width", height)
+    self.__width = width
+    self.__height = height
