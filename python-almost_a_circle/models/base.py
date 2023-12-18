0fc1ef2955b2c3ad23b19c7d20e1502a6cc1f56f
Shemaremy <remyshema20@gmail.com>
2023-12-18 15:13:49 +0200

    commit 867

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,29 +6,14 @@ everything will be included inside here
 class Base:
     """
     A class representing a Base object.
-
-    Attributes:
-    - __nb_objects (int): A private class attribute to keep track of the number of objects created.
-    - id (int): A public instance attribute representing the object's identifier.
-
-    Methods:
-    - __init__: The constructor method to initialize the Base object.
     """
 
     __nb_objects = 0 # private class attribute
 
     def __init__(self, id=None):
         """
-        Initializes a Base object.
-
-        Parameters:
-        - id (int): An optional parameter representing the identifier for the object.
-
-        If id is not provided:
-        - Increments __nb_objects.
-        - Assigns the new value of __nb_objects to the public instance attribute id.
-        If id is provided:
-        - Assigns the provided id to the public instance attribute id.
+        Initializes a Base object
+        and assigns the provided id to the public instance attribute id
         """
         if id is not None:
             self.id = id
@@ -36,9 +21,4 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-# Example usage:
-#obj1 = Base()
-#print(obj1.id) # Output: 1
 
-#obj2 = Base(100)
-#print(obj2.id) # Output: 100
