145a52d100f2b8172701954fc1f471fe095a6372
Shemaremy <remyshema20@gmail.com>
2023-12-19 20:31:01 +0200

    commit 726

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ url = "https://alu-intranet.hbtn.io/status"
 
 response = requests.get(url)
 print("Body response:")
-print(f"        - type:{type(response.text)}")
+print(f"        - type: {type(response.text)}")
 print(f"        - content: {response.text}")
