4c440378f0d9a49bc5538ad46e2cf7a689268f62
Shemaremy <remyshema20@gmail.com>
2023-12-21 19:50:58 +0200

    commit 389

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,1 +1,19 @@
+import requests
+import sys
 
+url = "http://0.0.0.0:5000/search_user"
+q = sys.argv[1] if len(sys.argv) > 1 else ""
+resp = requests.post(url, data={'q': q})
+
+try:
+    js = resp.json()
+    if js:
+        uid = js.get('id', '')
+        uname = js.get('name', '')
+        print(f"[{uid}]{uname}" if uid and uname else "No result")
+    
+    else:
+        print("No result")
+
+except ValueError:
+    print("Not a valid JSON")
