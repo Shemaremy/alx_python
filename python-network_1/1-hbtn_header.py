2d2f4f46610354d325c07e8ea85cd15128ac6fdd
Shemaremy <remyshema20@gmail.com>
2023-12-21 18:59:11 +0200

    commit 647

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,4 +4,4 @@ import sys
 url = sys.argv[1]
 resp = requests.get(url)
 m = resp.headers.get('X-Request-Id')
-print(f"X-Request-Id: {m}" if m else "X-Request-Id not found")
+print(f"{m}" if m else "None")
