aaf16e420a3942486d9aeccccc7827bf84cdb57f
Shemaremy <remyshema20@gmail.com>
2023-12-21 20:03:14 +0200

    commit 392

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,1 +1,18 @@
+import requests
+import sys
+
+username = sys.argv[1]
+access_token = sys.argv[2]
+url = "https://api.github.com/user"
+auth = (username, access_token)
+
+resp = requests.get(url, auth=auth)
+
+if resp.status_code == 200:
+    udata = resp.json()
+    uid = udata.get('id', '')
+    print(f"Your Id; {uid}" if uid else "Unable")
+
+else:
+    print(f"Error: {resp.status_code}")
 
