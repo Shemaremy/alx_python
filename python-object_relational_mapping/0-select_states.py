ca7b331a5a7af335867d14e58732e234231abbd0
Shemaremy <remyshema20@gmail.com>
2024-01-14 20:47:22 +0200

    commit 293

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,38 @@
+import MySQLdb
+import sys
+
+def list_states(username, password, database):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the query to retrieve the states from the hbtn_0e_0_usa database
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows
+    states = cursor.fetchall()
+
+    # Display the results
+    for state in states:
+        print(state)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Check if all three arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+    else:
+        # Extract arguments
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
+        # Call the function to list states
+        list_states(mysql_username, mysql_password, database_name)
 
