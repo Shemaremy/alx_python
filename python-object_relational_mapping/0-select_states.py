2ad3da267d1ccb5ba73d9f222ae186991e3fcb05
Remy <remyshema20@gmail.com>
2024-01-15 14:17:12 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,10 @@ import sys
 
 def list_states(username, password, database):
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    flacko = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, flacko=database)
 
     # Create a cursor object to interact with the database
-    cursor = db.cursor()
+    cursor = flacko.cursor()
 
     # Executing the query to retrieve the states from the hbtn_0e_0_usa database
     query = "SELECT * FROM states ORDER BY states.id ASC"
@@ -21,7 +21,7 @@ def list_states(username, password, database):
 
     # Close the cursor and database connection
     cursor.close()
-    db.close()
+    flacko.close()
 
 if __name__ == "__main__":
     # Check if all three arguments are provided
