80d3631e2f312cf357e0c25d90385193da5275b4
Shemaremy <remyshema20@gmail.com>
2024-01-17 23:12:26 +0200

    commit 568

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,18 @@
 import MySQLdb
 import sys
 
-def list_states_starting_with_n(username, password, database):
+def list_states(username, password, database):
     # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = 'New York'"
+    
+    # Define a SQL query to retrieve all columns (*) from the states table
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+
+    # Execute the SQL query using the cursor
     cursor.execute(query)
+
+    # Fetch all the rows returned by the query
     states = cursor.fetchall()
 
     # Display the results
@@ -27,5 +33,5 @@ if __name__ == "__main__":
         mysql_password = sys.argv[2]
         database_name = sys.argv[3]
 
-        # Call the function to list only 'New York'
-        list_states_starting_with_n(mysql_username, mysql_password, database_name)
+        # Call the function to list all states
+        list_states(mysql_username, mysql_password, database_name)
