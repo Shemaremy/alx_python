0614a15c5310ad48e69f6d55e39199b12b5bd9b5
Remy <remyshema20@gmail.com>
2024-01-20 11:23:56 +0200

    Update Understand_routes.py

--- python-web_framework/Understand_routes.py
+++ python-web_framework/Understand_routes.py
@@ -15,3 +15,29 @@ def about():
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+
+
+
+'''
+These codes illutstrates that when you run the url
+called imrunningcode, the output you will see 
+will be the prompt returned by the function 
+called home() .
+
+And by running the url about, also you will get 
+the prompt returned by the function about()
+
+
+
+
+In order to see the output, ensuring you have 
+flask installed, visit:
+
+-     http://127.0.0.1:5000/imrunningcode 
+-     http://127.0.0.1:5000/about
+
+
+
+
+'''
