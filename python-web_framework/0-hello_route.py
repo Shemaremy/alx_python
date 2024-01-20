fe657b1c133bfd14c402ddfa6325ee39cc519c7e
Shemaremy <remyshema20@gmail.com>
2024-01-20 17:12:23 +0200

    commit 546

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,3 +1,9 @@
+'''
+By here, we are  importing the Flask class from the flask web framework. 
+Flask is a micro web framework for Python that simplifies the process of building web applications. 
+The Flask class is a core component of Flask and is used to create a web application instance.
+
+'''
 from flask import Flask
 
 # Create a Flask web application instance
@@ -6,9 +12,11 @@ app = Flask(__name__)
 # Define the route for the root URL ("/") with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    # Handler function for the root route, returns the response "Hello HBNB!"
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
     # Run the Flask app on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
+
+
+
