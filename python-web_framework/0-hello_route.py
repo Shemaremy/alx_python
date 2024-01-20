03f23243f9fc25879cf8ae6e0390455fe82bd941
Remy <remyshema20@gmail.com>
2024-01-20 11:13:28 +0200

    Update 0-hello_route.py

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,1 +1,18 @@
 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/imrunningcode')
+def home():
+    return 'Hello, this is the imrunningcode page!'
+
+@app.route('/about')
+def about():
+    return 'Welcome to the about page!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
