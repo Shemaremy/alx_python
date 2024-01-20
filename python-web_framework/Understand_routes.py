81296f375385dfcc5410c66c4fa2c974d108636f
Remy <remyshema20@gmail.com>
2024-01-20 11:16:31 +0200

    Create Understand_routes.py

---
+++ python-web_framework/Understand_routes.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/imrunningcode')
+def home():
+    return 'Hello, this is the imrunningcode page!'
+
+@app.route('/about')
+def about():
+    return 'Welcome to the about page!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
