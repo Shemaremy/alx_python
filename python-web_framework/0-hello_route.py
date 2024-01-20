affe9a7e961c769e6e444b220ce5c880b12bf3eb
Shemaremy <remyshema20@gmail.com>
2024-01-20 17:19:39 +0200

    hhbhb

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -6,17 +6,33 @@ The Flask class is a core component of Flask and is used to create a web applica
 '''
 from flask import Flask
 
-# Create a Flask web application instance
-app = Flask(__name__)
 
-# Define the route for the root URL ("/") with strict_slashes=False
-@app.route('/', strict_slashes=False)
+a = Flask(__name__)
+
+# In the question, they told us to use the option      'strict_slashes=False'      in our route definition
+'''
+When you set strict_slashes=False in a route definition, it tells Flask to treat 
+URLs with or without a trailing slash as the same. 
+This means that both /example and /example/ would lead to the same route handler.
+'''
+@a.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
     # Run the Flask app on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+
+
+
+
+
+                        # SUMMARY
 
 
 
