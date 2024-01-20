09e584a41481497a45eee519b7a2eedd52e5959f
Shemaremy <remyshema20@gmail.com>
2024-01-20 17:27:04 +0200

    commit 343

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -6,16 +6,14 @@ The Flask class is a core component of Flask and is used to create a web applica
 '''
 from flask import Flask
 
-# Creation of a Flask web application instance
 app = Flask(__name__)
 
-# Define the route for the root URL ("/") with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the Flask app on 0.0.0.0, port 5000
+     
     app.run(host='0.0.0.0', port=5000)
 
 
