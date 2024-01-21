f2cac8e4bc5c45ccc05044729bc4821abf04b232
Shemaremy <remyshema20@gmail.com>
2024-01-21 16:03:10 +0200

    commit 645

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -11,20 +11,21 @@ app = Flask(__name__)
 
 
 
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
     return 'Hello HBNB!'
 
-
-
-@app.route('/HBNB',strict_slashes=False)
+@app.route('/HBNB', strict_slashes=False)
 def HBNB():
     return 'HBNB'
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
     
 
 
