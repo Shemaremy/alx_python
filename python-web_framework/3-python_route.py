c5be69d9c5ac3283c1fa73cde011eca99acad395
Shemaremy <remyshema20@gmail.com>
2024-01-21 17:16:09 +0200

    commit 577

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -16,11 +16,9 @@ app = Flask(__name__)
 def Hello_HBNB():
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     return 'HBNB'
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def C(text):
@@ -34,11 +32,9 @@ def Python(text):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
-
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
     
 
