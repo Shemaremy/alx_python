6027368a8acf5e038ebc9b9edb593cbd2ea95dab
Shemaremy <remyshema20@gmail.com>
2024-01-21 16:47:22 +0200

    commit 345

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -25,6 +25,14 @@ def HBNB():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    # Replace underscores with spaces in the text variable
+    text = text.replace('_', ' ')
+    return ('C',text)
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
