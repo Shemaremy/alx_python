fe41e5d0c82696aad0bea472d3b50e7f50c0d87d
Shemaremy <remyshema20@gmail.com>
2024-01-21 15:39:41 +0200

    commit 535

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,25 @@
+'''
+Importing flask from Flask as usual
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+@app.route('/HBNB',strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+if __name__=__main__:
+    app.run(host='0.0.0.0', port='5000')
