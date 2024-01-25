5d5ee3612fb5467a5cd3de50d0d4d9aefe43e7a4
Shemaremy <remyshema20@gmail.com>
2024-01-25 20:26:55 +0200

    commit 537

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,4 +1,19 @@
 '''
+QUESTION (0):
+
+ Write a script that starts a Flask web application:
+
+ Your web application must be listening on 0.0.0.0, port 5000
+ Routes:
+ /: display “Hello HBNB!”
+ You must use the option strict_slashes=False in your route definition
+
+
+'''
+
+
+
+'''
   By here, we are  importing the Flask class from the flask web framework. 
   Flask is a micro web framework for Python that simplifies the process of building web applications. 
   The Flask class is a core component of Flask and is used to create a web application instance.
